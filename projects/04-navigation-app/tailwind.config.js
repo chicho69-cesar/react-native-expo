1244840518063ff0e5cc49ddc905b282218e1e0a
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-07-29 10:40:18 -0600

    :wrench: Add assets and deps

--- projects/04-navigation-app/tailwind.config.js
+++ projects/04-navigation-app/tailwind.config.js
@@ -5,9 +5,24 @@ module.exports = {
     "./components/**/*.{js,jsx,ts,tsx}",
     "./presentation/**/*.{js,jsx,ts,tsx}",
   ],
-  presets: [ require( "nativewind/preset" ) ],
+  presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#49129C',
+        secondary: {
+          DEFAULT: '#B40086',
+          100: '#C51297',
+          200: '#831266',
+        },
+        tertiary: '#EF2967'
+      },
+      fontFamily: {
+        'work-black': ['WorkSans-Black', 'sans-serif'],
+        'work-light': ['WorkSans-Light', 'sans-serif'],
+        'work-medium': ['WorkSans-Medium', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
