d2435111c24ac4467f0692e889e1689807e80184
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-07-29 08:47:14 -0600

    :art: Configure nativewind

---
+++ projects/04-navigation-app/app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View className='mt-10'>
+      <Text className='text-3xl font-bold text-blue-500'>
+        Hola Mundo
+      </Text>
+    </View>
+  )
+}
