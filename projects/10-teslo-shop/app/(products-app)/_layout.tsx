f676deb720dbce080db974b1de214d8b69ce347a
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-08-10 10:04:59 -0600

    :sparkles: Add auth pages

---
+++ projects/10-teslo-shop/app/(products-app)/_layout.tsx
@@ -0,0 +1,67 @@
+import { Redirect, Stack } from 'expo-router'
+import { useEffect } from 'react'
+import { ActivityIndicator, View } from 'react-native'
+
+import LogoutIconButton from '@/presentation/auth/components/LogoutIconButton'
+import { useAuthStore } from '@/presentation/auth/store/useAuthStore'
+import { useThemeColor } from '@/presentation/theme/hooks/useThemeColor'
+
+const CheckAuthenticationLayout = () => {
+  const { status, checkStatus } = useAuthStore()
+  const backgroundColor = useThemeColor({}, 'background')
+
+  useEffect(() => {
+    checkStatus()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  if (status === 'checking') {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginBottom: 5,
+        }}
+      >
+        <ActivityIndicator />
+      </View>
+    )
+  }
+
+  if (status === 'unauthenticated') {
+    return <Redirect href='/auth/login' />
+  }
+
+  return (
+    <Stack
+      screenOptions={{
+        headerShadowVisible: false,
+        headerStyle: {
+          backgroundColor: backgroundColor,
+        },
+        contentStyle: {
+          backgroundColor: backgroundColor,
+        },
+      }}
+    >
+      <Stack.Screen
+        name='(home)/index'
+        options={{
+          title: 'Productos',
+          headerLeft: () => <LogoutIconButton />,
+        }}
+      />
+
+      <Stack.Screen
+        name='product/[id]'
+        options={{
+          title: 'Producto',
+        }}
+      />
+    </Stack>
+  )
+}
+
+export default CheckAuthenticationLayout
--- projects/10-teslo-shop/app/_layout.tsx
+++ projects/10-teslo-shop/app/_layout.tsx
@@ -1,29 +1,64 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import 'react-native-reanimated'
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { useFonts } from 'expo-font'
+import { Stack } from 'expo-router'
+import * as SplashScreen from 'expo-splash-screen'
+import { useEffect } from 'react'
+
+import { useColorScheme } from '@/presentation/theme/hooks/useColorScheme'
+import { useThemeColor } from '@/presentation/theme/hooks/useThemeColor'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+
+SplashScreen.preventAutoHideAsync()
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+})
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme()
+  const backgroundColor = useThemeColor({}, 'background')
+
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+    // SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    KanitRegular: require('../assets/fonts/Kanit-Regular.ttf'),
+    KanitBold: require('../assets/fonts/Kanit-Bold.ttf'),
+    KanitThin: require('../assets/fonts/Kanit-Thin.ttf'),
+  })
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded])
 
   if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
+    return null
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    <GestureHandlerRootView style={{ backgroundColor: backgroundColor, flex: 1 }}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider
+          value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
+        >
+          <Stack
+            screenOptions={{
+              headerShown: false,
+            }}
+          ></Stack>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </GestureHandlerRootView>
+  )
 }
