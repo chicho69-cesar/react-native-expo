0c6feb573c594fcba45cd306ea4445f4db8217bb
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-08-10 10:04:28 -0600

    :sparkles: Add product components

---
+++ projects/10-teslo-shop/presentation/auth/components/LogoutIconButton.tsx
@@ -0,0 +1,17 @@
+import { useThemeColor } from '@/presentation/theme/hooks/useThemeColor'
+import { Ionicons } from '@expo/vector-icons'
+import { TouchableOpacity } from 'react-native'
+import { useAuthStore } from '../store/useAuthStore'
+
+const LogoutIconButton = () => {
+  const primaryColor = useThemeColor({}, 'primary')
+  const { logout } = useAuthStore()
+
+  return (
+    <TouchableOpacity style={{ marginRight: 8 }} onPress={logout}>
+      <Ionicons name='log-out-outline' size={24} color={primaryColor} />
+    </TouchableOpacity>
+  )
+}
+
+export default LogoutIconButton
